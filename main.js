ab2185d06253309c43eab526ae07337841df386f
Laura <laura_martin92@outlook.com>
2021-10-15 13:28:40 +0800

    Hamburger to x button animation

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 console.log("A3 is working!")
 gsap.registerPlugin(ScrollTrigger); //is this necessary??
+
+
+const hamburgerBtn = document.querySelector('.hamburger-btn')
+const primaryMenu = document.querySelector('.primary-menu')
+
+hamburgerBtn.addEventListener('click', () => {
+  primaryMenu.classList.toggle('show')
+})
