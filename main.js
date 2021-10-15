5995041b6fa40fad65ff1634575a65aa27bf06c9
Laura <laura_martin92@outlook.com>
2021-10-15 22:53:21 +0800

    Started on marquee using css animation.
    use gsap instead.

--- main.js
+++ main.js
@@ -2,9 +2,13 @@ console.log("A3 is working!")
 gsap.registerPlugin(ScrollTrigger); //is this necessary??
 
 
-const hamburgerBtn = document.querySelector('.hamburger-btn')
+// responsive menu
+// get the hamburger icon, and the menu-div
+const hamburgerBtn = document.querySelector(".hamburger")
 const primaryMenu = document.querySelector('.primary-menu')
 
+// on click of hamburger icon, toggle add/remove the class to show/hide the menu, and animate the hamburger into an x/vice-versa
 hamburgerBtn.addEventListener('click', () => {
   primaryMenu.classList.toggle('show')
+  hamburgerBtn.classList.toggle('is-active')
 })
