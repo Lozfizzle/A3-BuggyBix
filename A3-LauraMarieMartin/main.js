554d7ccb357ff882b1a663436435f3b13c0c21c2
Laura <laura_martin92@outlook.com>
2021-11-04 23:31:52 +0800

    Swiper incorporated into main file. no problems.

--- A3-LauraMarieMartin/main.js
+++ A3-LauraMarieMartin/main.js
@@ -136,6 +136,49 @@ const logoSwiper = new Swiper('.logo-swiper', {
   }
 });
 
+// testimonials from customers
+var testimonialImageSwiper = new Swiper (".testimonial-image-swiper", {
+  spaceBetween: 50,
+  slidesPerView: "auto",
+  speed: 1000,
+  // loop: true, unfortunately i couldnt get the infinite loop working in combination with controlling two swipers at the same time. 
+  centeredSlides: true, 
+  navigation: {
+      // note: I've swapped what controls the next/prev buttons as I changed the directions of this swiper to 'right to left'
+      // as i wanted the images off to the left side of the screen
+      // nextEl: '.swiper-button-next',
+      // prevEl: '.swiper-button-prev',
+      nextEl: '.button-next',
+      prevEl: '.button-prev',  
+  },
+  keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+  },
+})
+
+var swiperTestimonialText = new Swiper(".testimonial-text-swiper", {
+  slidesPerView: "auto",
+  spaceBetween: 30,
+  effect: "fade", //so the text faces in and out instead of moving like regular swiper 
+  fadeEffect: { 
+      crossFade: true 
+  },
+  // loop: true, 
+  allowTouchMove: false,    
+});
+
+// one swiper also controls the other swiper, so image and text are in sync
+// clicking to next slide on one swiper will also change the slide of the other swiper
+testimonialImageSwiper.controller.control = swiperTestimonialText;
+swiperTestimonialText.controller.control = testimonialImageSwiper;
+
+
+
+
+
+
+
 
 // subscribe submit form
 // i know nothing about server side programming
