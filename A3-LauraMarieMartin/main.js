7ed4dccd1a6e935f05d7f2823f830f44bcecc4c0
Laura <laura_martin92@outlook.com>
2021-11-05 19:52:03 +0800

    all in.

--- A3-LauraMarieMartin/main.js
+++ A3-LauraMarieMartin/main.js
@@ -1,5 +1,6 @@
 console.log("yo!")
 gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(MotionPathPlugin);
 
 
 // home section
@@ -84,26 +85,7 @@ function homeSection(){
     .from(".shop-sustainable", {y:30, duration: 1, opacity: 0}, "-=1")
 }
 
-
-
-
-
-
-
-// toggleAction: onEnter. onLeave. onEnterBack. onLeaveBack 
-      // default  play    none      none        none
-  // actions    play pause resume  reverse  restart reset complete none
-
-//scrub: true
-// or give it a number to smooth
-//scrub: 4 
-
-// can pin element in place 
-// pin: ".topbar"
-
-
-
-
+//================================ logo marquee ================================
 // logo marquee
 // using swiper js
 // logo marquee has infinite loop, and starts automatically
@@ -142,6 +124,85 @@ const logoSwiper = new Swiper('.logo-swiper', {
 });
 
 
+
+//================================ ingredients ================================
+// ingredients and benefits section
+// setting up the scrolltrigger to begin 
+const ingredientsTl = gsap.timeline({
+  scrollTrigger: {
+      trigger: '.ingredients-images',
+      start: 'top top',
+      pin: true,
+      scrub: 1,
+      end: "+=10000" // making the scrub animations take longer
+  },
+})
+
+// i know this doesnt look amazing on anything less than a desktop screen
+// but i couldnt figure out the gsap timeline breakpoints to trigger certain animations for different screensizes
+// next time
+
+// setting the background colour so it makes the transition to a different colour in the animation nice
+gsap.set('.section-ingredients', { backgroundColor: '#fff4e5' })
+gsap.set('.ingredient-packet', { opacity: 1,})
+
+// e4d5b7 darker beige
+
+// the animations for the packet to move to the side
+// and the text to appear one by one 
+ingredientsTl.to('.ingredients.content', { y: -50, opacity: 0, ease: "ease-in" })
+ingredientsTl.to('.section-ingredients', { backgroundColor: '#e4d5b7', duration: 4, ease: "linear" })
+ingredientsTl.to('.ingredient-packet', {x: "35%", y: "15vh", scale: 0.8, ease: "easeIn" })
+ingredientsTl.from('.in-txt1', { y: 100, duration: 4, opacity: 0, })
+ingredientsTl.to('.in-txt1', { y: -100, duration: 4, opacity: 0, delay: 4 })
+ingredientsTl.from('.in-txt2', { y: 100, duration: 4, opacity: 0 },)
+ingredientsTl.to('.in-txt2', { y: -100, duration: 4, opacity: 0, delay: 4 })
+ingredientsTl.from('.in-txt3', { y: 100, duration: 4, opacity: 0 },)
+ingredientsTl.to('.in-txt3', { y: -100, duration: 4, opacity: 0, delay: 4 })
+ingredientsTl.to('.ingredient-packet', { opacity: 0, })
+ingredientsTl.from('.in-txt4', { y: 100, duration: 4, opacity: 0 })
+ingredientsTl.to('.in-txt4', { y: -100, duration: 4, opacity: 0, delay: 4 })
+ingredientsTl.from('.in-txt5', { y: 100, duration: 4, opacity: 0 })
+ingredientsTl.to('.in-txt5', { y: -100, duration: 4, opacity: 0, delay: 4 })
+
+// animating a bug later
+// this is the path it will take 
+var flightpath = [
+  { x: 100, y: -20 },
+  { x: 300, y: 10 },
+  { x: 500, y: 100 },
+  { x: 750, y: -100 },
+  { x: 350, y: -50 },
+  { x: 600, y: 100 },
+  { x: 800, y: 0 },
+  { x: 2000, y: 100 }
+]
+
+// now the animation for the bug
+// on the same timeline as the rest of the section
+ingredientsTl.from('.cute-bug', {opacity: 0})
+ingredientsTl.to('.cute-bug', {
+  opacity: 1,
+  motionPath: {
+      path: flightpath, // this is the path of the coordinates we set above
+      autoRotate: true, // the bug will turn upside down when it does a loop =)
+      curviness: 1.5, // and it will curve on the path, not just turn at 90 degree angles
+  },
+  duration: 10, // i dont think this duration made any difference =/
+  ease: "power1.inOut",
+},
+)
+
+// continuing on the rest of the animation on the timeline
+// setting the last bit of text. it will appear at the same time as the flying bug
+ingredientsTl.from('.in-txt6', { y: 600, duration: 4, opacity: 0 }, "-=4")
+ingredientsTl.to('.in-txt6', { y: -100, duration: 4, opacity: 1},)
+ingredientsTl.to('.section-ingredients', { backgroundColor: '#ffffff', duration: 4, ease: "linear" })
+
+
+
+//================================ benefits ================================
+
 // benefits section animation 
 // when entering this section the heading will animate downwards
 // the circle illustrations and text will animate upwards in a canon
@@ -150,8 +211,6 @@ const benefitsTl = gsap.timeline({
   scrollTrigger: {
       trigger: ".section-benefits",
       start: "top 90%",
-      
-      markers: true,
       toggleActions: "play none restart restart",
   }, 
 })
@@ -160,13 +219,9 @@ benefitsTl.from(".section-benefits h1", {duration: 1, y: -50, opacity: 0, delay:
 benefitsTl.from(".benefit-column", {duration: 1, y: 30, opacity: 0, stagger: 0.3,},"-=0.5")
 
 
-
-
-
-
-
-
+//================================ testimonials ================================
 // testimonials from customers
+// this is two swipers next to each other that are connected - one shows the image and the other the accompnyng text
 const testimonialImageSwiper = new Swiper (".testimonial-image-swiper", {
   spaceBetween: 50,
   slidesPerView: "auto",
@@ -204,6 +259,10 @@ testimonialImageSwiper.controller.control = swiperTestimonialText;
 swiperTestimonialText.controller.control = testimonialImageSwiper;
 
 
+
+
+//================================ submit form ================================
+
 // subscribe gsap animation
 // when top of signup section hits 90% down the screen play the tl
 // onLeaveBack - reset the tl, so when you scroll back down the animation plays again
@@ -222,7 +281,6 @@ subscribeTl.from('.form sl-button', {x: 30, opacity: 0}, '-=1')
 
 
 
-
 // subscribe submit form
 // i know nothing about server side programming
 // but this ouputs the email submitted in the sl-input to the console =)
@@ -238,5 +296,16 @@ subscribeForm.addEventListener('sl-submit', event => {
   console.log(formData.get("email"))
   
 })
+
+// toggleAction: onEnter. onLeave. onEnterBack. onLeaveBack 
+      // default  play    none      none        none
+  // actions    play pause resume  reverse  restart reset complete none
+
+//scrub: true
+// or give it a number to smooth
+//scrub: 4 
+
+// can pin element in place 
+// pin: ".topbar"
 
 
--- benefits/main.js
+++ benefits/main.js
@@ -1,40 +1,81 @@
 console.log("JavaScript is working!")
-// TODO: 3 shoelace componenets have to be 3 different types. button, icon, something else! 
+gsap.registerPlugin(MotionPathPlugin);
 
 
+
+
+// ingredients and benefits section
+// setting up the scrolltrigger to begin 
 const ingredientsTl = gsap.timeline({
     scrollTrigger: {
         trigger: '.ingredients-images',
         start: 'top top',
         pin: true,
         scrub: 1,
-        end: "+=6000" // making the scrub animations take longer
-    },  
+        end: "+=10000" // making the scrub animations take longer
+    },
 })
 
-gsap.set('.section-ingredients', {backgroundColor: '#fff4e5'})
-// gsap.set('.ingredients.content', {y: 0, opacity: 1} )
+// i know this doesnt look amazing on anything less than a desktop screen
+// but i couldnt figure out the gsap timeline breakpoints to trigger certain animations for different screensizes
+// next time
+
+// setting the background colour so it makes the transition to a different colour in the animation nice
+gsap.set('.section-ingredients', { backgroundColor: '#fff4e5' })
+gsap.set('.ingredient-packet', { opacity: 1,})
+
 // e4d5b7 darker beige
 
-ingredientsTl.to('.ingredients.content', {y: -50, opacity: 0, ease: "ease-in"})
-ingredientsTl.to('.section-ingredients', {backgroundColor: 'beige', duration: 4, ease: "linear"})
-ingredientsTl.to('.ingredient-packet', {x: '30%', scale: 0.8,})
-ingredientsTl.from('.in-txt1', {y: 100, duration: 4, opacity: 0,})
-ingredientsTl.to('.in-txt1', {y: -100, duration: 4, opacity: 0, delay: 4})
-ingredientsTl.from('.in-txt2', {y: 100, duration: 4, opacity: 0},)
-ingredientsTl.to('.in-txt2', {y: -100, duration: 4, opacity: 0, delay: 4})
-ingredientsTl.from('.in-txt3', {y: 100, duration: 4, opacity: 0},)
-ingredientsTl.to('.in-txt3', {y: -100, duration: 4, opacity: 0, delay: 4})
-ingredientsTl.to('.ingredient-packet', {opacity: 0,}) 
-ingredientsTl.from('.in-txt4', {y: 100, duration: 4, opacity: 0})
-ingredientsTl.to('.in-txt4', {y: -100, duration: 4, opacity: 0, delay: 4})
-ingredientsTl.from('.in-txt5', {y: 100, duration: 4, opacity: 0})
-ingredientsTl.to('.in-txt5', {y: -100, duration: 4, opacity: 0, delay: 4})
-ingredientsTl.from('.in-txt6', {y: 100, duration: 4, opacity: 0})
-ingredientsTl.to('.in-txt6', {y: -100, duration: 4, opacity: 1, delay: 4})
-ingredientsTl.to('.section-ingredients', {backgroundColor: '#ffffff', duration: 4, ease: "linear"})
+// the animations for the packet to move to the side
+// and the text to appear one by one 
+ingredientsTl.to('.ingredients.content', { y: -50, opacity: 0, ease: "ease-in" })
+ingredientsTl.to('.section-ingredients', { backgroundColor: '#e4d5b7', duration: 4, ease: "linear" })
+ingredientsTl.to('.ingredient-packet', {x: "35%", y: "15vh", scale: 0.8, ease: "easeIn" })
+ingredientsTl.from('.in-txt1', { y: 100, duration: 4, opacity: 0, })
+ingredientsTl.to('.in-txt1', { y: -100, duration: 4, opacity: 0, delay: 4 })
+ingredientsTl.from('.in-txt2', { y: 100, duration: 4, opacity: 0 },)
+ingredientsTl.to('.in-txt2', { y: -100, duration: 4, opacity: 0, delay: 4 })
+ingredientsTl.from('.in-txt3', { y: 100, duration: 4, opacity: 0 },)
+ingredientsTl.to('.in-txt3', { y: -100, duration: 4, opacity: 0, delay: 4 })
+ingredientsTl.to('.ingredient-packet', { opacity: 0, })
+ingredientsTl.from('.in-txt4', { y: 100, duration: 4, opacity: 0 })
+ingredientsTl.to('.in-txt4', { y: -100, duration: 4, opacity: 0, delay: 4 })
+ingredientsTl.from('.in-txt5', { y: 100, duration: 4, opacity: 0 })
+ingredientsTl.to('.in-txt5', { y: -100, duration: 4, opacity: 0, delay: 4 })
 
+// animating a bug later
+// this is the path it will take 
+var flightpath = [
+    { x: 100, y: -20 },
+    { x: 300, y: 10 },
+    { x: 500, y: 100 },
+    { x: 750, y: -100 },
+    { x: 350, y: -50 },
+    { x: 600, y: 100 },
+    { x: 800, y: 0 },
+    { x: 2000, y: 100 }
+]
 
+// now the animation for the bug
+// on the same timeline as the rest of the section
+ingredientsTl.from('.cute-bug', {opacity: 0})
+ingredientsTl.to('.cute-bug', {
+    opacity: 1,
+    motionPath: {
+        path: flightpath, // this is the path of the coordinates we set above
+        autoRotate: true, // the bug will turn upside down when it does a loop =)
+        curviness: 1.5, // and it will curve on the path, not just turn at 90 degree angles
+    },
+    duration: 10, // i dont think this duration made any difference =/
+    ease: "power1.inOut",
+},
+)
+
+// continuing on the rest of the animation on the timeline
+// setting the last bit of text. it will appear at the same time as the flying bug
+ingredientsTl.from('.in-txt6', { y: 600, duration: 4, opacity: 0 }, "-=4")
+ingredientsTl.to('.in-txt6', { y: -100, duration: 4, opacity: 1},)
+ingredientsTl.to('.section-ingredients', { backgroundColor: '#ffffff', duration: 4, ease: "linear" })
 
 
 
@@ -42,18 +83,57 @@ ingredientsTl.to('.section-ingredients', {backgroundColor: '#ffffff', duration: 
 // when entering this section the heading will animate downwards
 // the circle illustrations and text will animate upwards in a canon
 // this will happen everytime user enters the section
-const benefitsTl = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".section-benefits",
-        start: "top bottom",
-        endTrigger: ".section-benefits",
-        end: "top top",
-        markers: true,
-        toggleActions: "play none none restart",
-        //TODO: make sure these togge actions are correct when all together
-    }, 
-})
+// const benefitsTl = gsap.timeline({
+//     scrollTrigger: {
+//         trigger: ".section-benefits",
+//         start: "top bottom",
+//         endTrigger: ".section-benefits",
+//         end: "top top",
+//         markers: true,
+//         toggleActions: "play none none restart",
+//         //TODO: make sure these togge actions are correct when all together
+//     }, 
+// })
 
-benefitsTl.from(".section-benefits h1", {duration: 1, y: -50, opacity: 0,})
-benefitsTl.from(".benefit-column", {duration: 1, y: 30, opacity: 0, stagger: 0.3},"-=0.5")
+// benefitsTl.from(".section-benefits h1", {duration: 1, y: -50, opacity: 0,})
+// benefitsTl.from(".benefit-column", {duration: 1, y: 30, opacity: 0, stagger: 0.3},"-=0.5")
 
+
+
+
+
+// the og timeline
+
+
+// const ingredientsTl = gsap.timeline({
+//     scrollTrigger: {
+//         trigger: '.ingredients-images',
+//         start: 'top top',
+//         pin: true,
+//         scrub: 1,
+//         end: "+=6000" // making the scrub animations take longer
+//     },  
+// })
+
+// gsap.set('.section-ingredients', {backgroundColor: '#fff4e5'})
+// gsap.set('.ingredient-packet', {opacity: 1})
+// // gsap.set('.ingredients.content', {y: 0, opacity: 1} )
+// // e4d5b7 darker beige
+
+// ingredientsTl.to('.ingredients.content', {y: -50, opacity: 0, ease: "ease-in"})
+// ingredientsTl.to('.section-ingredients', {backgroundColor: 'beige', duration: 4, ease: "linear"})
+// ingredientsTl.to('.ingredient-packet', {x: '30%', scale: 0.8,})
+// ingredientsTl.from('.in-txt1', {y: 100, duration: 4, opacity: 0,})
+// ingredientsTl.to('.in-txt1', {y: -100, duration: 4, opacity: 0, delay: 4})
+// ingredientsTl.from('.in-txt2', {y: 100, duration: 4, opacity: 0},)
+// ingredientsTl.to('.in-txt2', {y: -100, duration: 4, opacity: 0, delay: 4})
+// ingredientsTl.from('.in-txt3', {y: 100, duration: 4, opacity: 0},)
+// ingredientsTl.to('.in-txt3', {y: -100, duration: 4, opacity: 0, delay: 4})
+// ingredientsTl.to('.ingredient-packet', {opacity: 0,}) 
+// ingredientsTl.from('.in-txt4', {y: 100, duration: 4, opacity: 0})
+// ingredientsTl.to('.in-txt4', {y: -100, duration: 4, opacity: 0, delay: 4})
+// ingredientsTl.from('.in-txt5', {y: 100, duration: 4, opacity: 0})
+// ingredientsTl.to('.in-txt5', {y: -100, duration: 4, opacity: 0, delay: 4})
+// ingredientsTl.from('.in-txt6', {y: 100, duration: 4, opacity: 0})
+// ingredientsTl.to('.in-txt6', {y: -100, duration: 4, opacity: 1, delay: 4})
+// ingredientsTl.to('.section-ingredients', {backgroundColor: '#ffffff', duration: 4, ease: "linear"})
